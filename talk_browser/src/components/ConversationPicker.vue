5e2c33f5f8d7e85f60ba7f489b40ae2392e7669e
Yannick Lescure <yannick.lescure@gmail.com>
2026-03-31 20:28:20 -0400

    feat: add conversation search filter to sidebar

--- talk_browser/src/components/ConversationPicker.vue
+++ talk_browser/src/components/ConversationPicker.vue
@@ -4,54 +4,75 @@
 			<NcLoadingIcon :size="20" />
 		</div>
 
-		<ul v-else class="conversation-list__items">
-			<li
-				v-for="conv in conversations"
-				:key="conv.token"
-				:class="[
-					'conversation-list__item',
-					{ 'conversation-list__item--active': conv.token === value },
-				]"
-				@click="$emit('input', conv.token)"
-			>
-			<!-- Avatar: user avatar for 1:1; current user for note-to-self; room avatar for group/public -->
-			<NcAvatar
-				v-if="conv.type === 1"
-				:user="conv.name"
-				:display-name="conv.displayName"
-				:size="32"
-				:show-user-status="false"
-				class="conversation-list__avatar"
+		<template v-else>
+			<NcTextField
+				v-model="search"
+				:label="t('talk_browser', 'Search conversations')"
+				:placeholder="t('talk_browser', 'Search…')"
+				class="conversation-list__search"
+				trailing-button-icon="close"
+				:show-trailing-button="search !== ''"
+				@trailing-button-click="search = ''"
 			/>
-			<NcAvatar
-				v-else-if="conv.type === 6"
-				:user="currentUser.uid"
-				:display-name="currentUser.displayName"
-				:size="32"
-				:show-user-status="false"
-				class="conversation-list__avatar"
-			/>
-				<NcAvatar
-					v-else
-					:url="roomAvatarUrl(conv)"
-					:display-name="conv.displayName"
-					:size="32"
-					:is-no-user="true"
-					class="conversation-list__avatar"
-				/>
 
-				<!-- Name + type badge -->
-				<span class="conversation-list__name">{{ conv.displayName }}</span>
-				<span v-if="conv.type === 6" class="conversation-list__badge">
-					{{ t('talk_browser', 'You') }}
-				</span>
-			</li>
-		</ul>
+			<NcEmptyContent
+				v-if="filtered.length === 0"
+				:name="t('talk_browser', 'No conversations found')"
+			>
+				<template #icon>
+					<span class="icon-search" />
+				</template>
+			</NcEmptyContent>
+
+			<ul v-else class="conversation-list__items">
+				<li
+					v-for="conv in filtered"
+					:key="conv.token"
+					:class="[
+						'conversation-list__item',
+						{ 'conversation-list__item--active': conv.token === value },
+					]"
+					@click="$emit('input', conv.token)"
+				>
+					<!-- Avatar: user avatar for 1:1; current user for note-to-self; room avatar for group/public -->
+					<NcAvatar
+						v-if="conv.type === 1"
+						:user="conv.name"
+						:display-name="conv.displayName"
+						:size="32"
+						:show-user-status="false"
+						class="conversation-list__avatar"
+					/>
+					<NcAvatar
+						v-else-if="conv.type === 6"
+						:user="currentUser.uid"
+						:display-name="currentUser.displayName"
+						:size="32"
+						:show-user-status="false"
+						class="conversation-list__avatar"
+					/>
+					<NcAvatar
+						v-else
+						:url="roomAvatarUrl(conv)"
+						:display-name="conv.displayName"
+						:size="32"
+						:is-no-user="true"
+						class="conversation-list__avatar"
+					/>
+
+					<!-- Name + type badge -->
+					<span class="conversation-list__name">{{ conv.displayName }}</span>
+					<span v-if="conv.type === 6" class="conversation-list__badge">
+						{{ t('talk_browser', 'You') }}
+					</span>
+				</li>
+			</ul>
+		</template>
 	</div>
 </template>
 
 <script>
-import { NcAvatar, NcLoadingIcon } from '@nextcloud/vue'
+import { NcAvatar, NcEmptyContent, NcLoadingIcon, NcTextField } from '@nextcloud/vue'
 import { getRootUrl } from '@nextcloud/router'
 import { translate as t } from '@nextcloud/l10n'
 import { getCurrentUser } from '@nextcloud/auth'
@@ -59,7 +80,7 @@ import { getCurrentUser } from '@nextcloud/auth'
 export default {
 	name: 'ConversationPicker',
 
-	components: { NcAvatar, NcLoadingIcon },
+	components: { NcAvatar, NcEmptyContent, NcLoadingIcon, NcTextField },
 
 	props: {
 		conversations: {
@@ -79,7 +100,18 @@ export default {
 	data() {
 		return {
 			currentUser: getCurrentUser() || { uid: '', displayName: '' },
+			search: '',
 		}
+	},
+
+	computed: {
+		filtered() {
+			if (!this.search) return this.conversations
+			const q = this.search.toLowerCase()
+			return this.conversations.filter(c =>
+				c.displayName?.toLowerCase().includes(q),
+			)
+		},
 	},
 
 	methods: {
@@ -107,6 +139,10 @@ export default {
 	display: flex;
 	justify-content: center;
 	padding: 16px 0;
+}
+
+.conversation-list__search {
+	margin: 4px 8px 8px;
 }
 
 .conversation-list__items {
